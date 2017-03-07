171cd9611f0fc320eddc55b813da39dec0084aa2
Pahnki Kim <pahnki.kim@pkworks.io>
2017-03-07 16:11:37 +0900

    update admin ui

--- src/main/java/com/hanbit/hp/admin/AdminController.java
+++ src/main/java/com/hanbit/hp/admin/AdminController.java
@@ -2,6 +2,7 @@ package com.hanbit.hp.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +15,15 @@ public class AdminController {
 		model.addAttribute("jsName", "main");
 		
 		return "admin/index";
+	}
+	
+	@RequestMapping("/{menuId}")
+	public String menu(Model model, @PathVariable("menuId") String menuId) {
+		model.addAttribute("menuId", menuId);
+		model.addAttribute("cssName", "main");
+		model.addAttribute("jsName", "main");
+		
+		return "admin/" + menuId;
 	}
 	
 }
--- src/main/resources/META-INF/resources/js/common.js
+++ src/main/resources/META-INF/resources/js/common.js
@@ -1,5 +1,35 @@
 define([
 	"bootstrap",
 ], function() {
+	$(".admin-logo").on("click", function() {
+		location.href = "/admin/";
+	});
 	
+	$(".admin-menu>ul>li").on("click", function() {
+		var menu = $(this).attr("menu");
+		
+		location.href = "/admin/" + menu;
+	});
+	
+	if (_ctx.menuId && _ctx.menuId !== "") {
+		$(".admin-menu>ul>li[menu='" + _ctx.menuId + "']")
+			.addClass("active");
+	}
+	
+	function showSection(section) {
+		$("section").hide();
+		$(section).show();
+	}
+	
+	$(".btn-admin-add").on("click", function() {
+		showSection(".admin-add");
+	});
+	
+	$(".btn-admin-cancel").on("click", function() {
+		showSection(".admin-list");
+	});
+	
+	$(".admin-list table>tbody>tr").on("click", function() {
+		showSection(".admin-update");
+	});
 });
